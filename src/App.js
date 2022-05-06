12beb5c730da9fbbab144748a9e88a0681ee9324
Anirban barman <anirbandali247@gmail.com>
2022-05-06 12:17:25 +0530

    add html css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [avatarUrl,setAvatarUrl]=useState("")
+  const [__avatarUrl,__setAvatarUrl]=useState({name:""})
+
+  useEffect(() => {
+    setAvatarUrl("")
+
+  
+    const subdomain = 'demo'; // Replace with your custom subdomain
+    const frame = document.getElementById('frame');
+     frame.src = `https://${subdomain}.readyplayer.me/avatar?frameApi`;
+     function subscribe(event) {
+      const json = parse(event);
+
+      if (json?.source !== 'readyplayerme') {
+        return;
+      }
+
+      // Susbribe to all events sent from Ready Player Me once frame is ready
+      if (json.eventName === 'v1.frame.ready') {
+      
+        frame.contentWindow.postMessage(
+          JSON.stringify({
+            target: 'readyplayerme',
+            type: 'subscribe',
+            eventName: 'v1.**'
+          }),
+          '*'
+        );
+      }
+
+      // Get avatar GLB URL
+      if (json.eventName === 'v1.avatar.exported') {
+        
+ 
+ 
+        console.log(`Avatar URL: ${json.data.url}`);
+        setAvatarUrl(json.data.url)
+        __setAvatarUrl({name:json.data.url})
+        // document.getElementById('frame').hidden = true;
+        // displayIframe();
+     
+      }
+
+      // Get user id
+      if (json.eventName === 'v1.user.set') {
+   
+        console.log(`User with id ${json.data.id} set: ${JSON.stringify(json)}`);
+      }
+    }
+
+    function parse(event) {
+      try {
+        return JSON.parse(event.data);
+      } catch (error) {
+        return null;
+      }
+    }
+
+    function displayIframe() {
+      document.getElementById('frame').hidden = false;
+    }
+
+     
+    window.addEventListener('message', subscribe);
+    document.addEventListener('message', subscribe);
+    return () => {
+      window.removeEventListener('message', subscribe);
+      document.removeEventListener('message', subscribe);
+    };
+
+    
+
+    
+
+  },
+ [avatarUrl,setAvatarUrl]);
+  return (<>
+      {__avatarUrl.name && <div> <h3 style={{textAlign:"center"}}>{`Your Avatar url is `} : <mark>{`${__avatarUrl.name}`}</mark></h3></div>}
+    <div style={{display:'flex',justifyContent:"center",alignItems:"center"}} >
+      <iframe title='avatar' id="frame" className="frame" allow="camera *; microphone *"></iframe>
+
     </div>
+    </>
   );
 }
 
